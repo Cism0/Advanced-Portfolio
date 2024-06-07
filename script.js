049ca046485877019b021a55ffd9d9c9e6cf62d8
Your Name <you@example.com>
2024-06-07 13:38:38 -0400

    Finished version of my E-porfolio

---
+++ script.js
@@ -0,0 +1,62 @@
+// template_f14gyej
+// service_xvmz064
+// 4jTvjL_6Sl8vPrKSE
+
+let isModalOpen = false;
+let contrastToggle = false;
+const scaleFactor = 1 / 20;
+
+function moveBackground(event) {
+  const shapes = document.querySelectorAll(".shape");
+  const x = event.clientX * scaleFactor;
+  const y = event.clientY * scaleFactor;
+
+  for (let i = 0; i < shapes.length; ++i) {
+    // the  QUOTES have to be ` not " or '
+    const isOdd = i % 2 !== 0;
+    const boolInt = isOdd ? -1 : 1;
+    shapes[i].style.transform = `translate(${x * boolInt}px, ${y * boolInt}px)`;
+  }
+}
+
+function toggleContrast() {
+  contrastToggle = !contrastToggle;
+  if (contrastToggle) {
+    document.body.classList += " dark__theme";
+  } else {
+    document.body.classList.remove("dark__theme");
+  }
+}
+
+function contact(event) {
+  event.preventDefault();
+  const loading = document.querySelector(".modal__overlay--loading");
+  const success = document.querySelector(".modal__overlay--success");
+  loading.classList += " modal__overlay--visible";
+  emailjs
+    .sendForm(
+      "service_xvmz064",
+      "template_f14gyej",
+      event.target,
+      "4jTvjL_6Sl8vPrKSE"
+    )
+    .then(() => {
+      loading.classList.remove("modal__overlay--visible");
+      success.classList += " modal__overlay--visible";
+    })
+    .catch(() => {
+      loading.classList.remove("modal__overlay--visible");
+      alert(
+        "The email service is temporarily unavailable. Please contact me directly on garabedianchristopher015@email.com"
+      );
+    });
+}
+
+function toggleModal() {
+  if (isModalOpen) {
+    isModalOpen = false;
+    return document.body.classList.remove("modal__open");
+  }
+  isModalOpen = true;
+  document.body.classList += " modal__open";
+}
